ae95360d66b356bf39457a51a1476e2588606a59
RnR-Han <99482061+RnR-Han@users.noreply.github.com>
2024-03-02 08:27:00 +0700

    Add files via upload

---
+++ Portofolio/script.js
@@ -0,0 +1,13 @@
+let text = document.querySelector(".text");
+
+let typed = new Typed(text, {
+    strings : [
+        "Full Stack Developer", 
+        "Frond End Developer", 
+        "Back End Develover"
+    ],
+    speed: 50,
+    backSpeed: 30,
+    backDelay:1000,
+    loop:true
+});
